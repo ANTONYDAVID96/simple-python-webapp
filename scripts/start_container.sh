6b0fcddfef06643ea54ddd2d00d0817b8cdc0119
ANTONYDAVID96 <139280805+ANTONYDAVID96@users.noreply.github.com>
2024-02-15 23:36:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker pull simple_py_app:1
-sudo docker run -d -p 3000:3000 simple_py_app:1
+set -e
+docker pull antonydavid96/simple_py_app:1
+docker run -d -p 3000:3000 antonydavid96/simple_py_app:1
